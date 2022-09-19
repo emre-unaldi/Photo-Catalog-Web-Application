4faee4dd3c8c7eb064bc1c138e1cbbfff3f0e2ca
emre-unaldi <emree.unaldi@gmail.com>
2022-09-19 20:26:04 +0300

    Setting up mongodb connection and database edits

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./Models/Photo');
+
 const app = express();
 const PORT = 3000;
+
+// Connect DB
+mongoose.connect('mongodb://localhost/photo-catalog', {
+    useNewUrlParser : true,
+    useUnifiedTopology : true 
+})
 
 // Template Engine 
 app.set("view engine", "ejs");
@@ -11,9 +20,15 @@ app.set("view engine", "ejs");
 // middleware yazdıktan sonra diğer middleware geçmek için (req, res, next) next() yazılmalıdır.
 // Middlewares
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
-app.get('/', (req,res) => {
-    res.render('index')
+// Routes
+app.get('/', async (req,res) => {
+    const photos = await Photo.find({}); 
+    res.render('index', {
+        photos
+    })
 });
 
 app.get('/about', (req,res) => {
@@ -22,6 +37,11 @@ app.get('/about', (req,res) => {
 
 app.get('/add', (req,res) => {
     res.render('add')
+});
+
+app.post('/photos', async (req,res) => {
+    await Photo.create(req.body);
+    res.redirect('/');
 });
 
 app.listen(PORT, () => {
